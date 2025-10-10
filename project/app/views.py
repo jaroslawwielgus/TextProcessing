4d471741175f24766f73ea9b650ed2d1d0b05fbe
jaroslawwielgus <jaroslaw.wielgus7557@gmail.com>
2025-10-10 18:30:12 +0200

    form - uploading file and navigating to score view ready

--- project/app/views.py
+++ project/app/views.py
@@ -1,9 +1,16 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+
+from .forms import FileForm
 
 # Create your views here.
 def form(request):
-    return render(request, "form.html")
+    form = FileForm()
+    return render(request, "form.html", {"form": form})
 
 def score(request):
+    if request.method == "POST":
+        return HttpResponseRedirect("/wynik")
     return render(request, "score.html")
+
 
