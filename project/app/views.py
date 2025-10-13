21f8a70bc5bf9358c1bf73d6580f4a6dde2fa08d
jaroslawwielgus <jaroslaw.wielgus7557@gmail.com>
2025-10-13 12:26:47 +0200

    handling file - processing works ok

--- project/app/views.py
+++ project/app/views.py
@@ -1,7 +1,6 @@
-from django.http import HttpResponseRedirect
 from django.shortcuts import render
-
 from .forms import FileForm
+import re, random
 
 # Create your views here.
 def form(request):
@@ -10,7 +9,28 @@ def form(request):
 
 def score(request):
     if request.method == "POST":
-        return HttpResponseRedirect("/wynik")
+        uploaded_file = request.FILES['file']
+        text = ''
+
+        for line in uploaded_file:
+            text += line.decode()
+
+        pattern = r"[a-żA-Ż-']+"
+
+        def shuffler(match):
+            word = match.group(0)  
+
+            if len(word) > 3:
+                word_center = word[1:-1]
+                chars = list(word_center)
+                random.shuffle(chars)
+                return word[0] + "".join(chars) + word[-1]
+            else:
+                return word
+
+        new_text = re.sub(pattern, shuffler, text)
+        return render(request, "score.html", {"text": new_text})
+
     return render(request, "score.html")
 
 
